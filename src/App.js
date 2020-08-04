65e8dbede4168c5ed74e31d27055ec65d2a130ea
Kevin Tu <ktu@jkevintu.com>
2020-08-04 13:46:20 +0800

    Create page:Home component using react route

--- src/App.js
+++ src/App.js
@@ -1,70 +1,16 @@
 import React from 'react';
-import Layout from "./components/Layout"
-import Hero from "./components/Hero"
-import Section from "./components/Section"
-import ImageItem from "./components/ImageItem"
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
 // import logo from './logo.svg';
 // import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <Layout>
-        <Hero isHomepage="true"></Hero>
-        <Section sectionId="about">
-          <h2>About</h2>
-          <code>
-            <strong>J.Kevin Tu</strong><br/>
-            Frontend Developer / Consultant<br/>
-            -----------------------------------<br/>
-            HTML/CSS<br/>
-            Javascript (Vue, React, Angular)<br/>
-            UI/UX/Branding (Photoshop, Sktech)<br/>
-            SEO, SEM, Google Analytics<br/>
-            508 Compliance<br/>
-          </code>
-          <p>
-            <a href="mailto:ktu@jkevintu.com">ktu@jkevintu.com</a><br/><br/>
-            <a href="https://www.linkedin.com/in/jkevintu">>> linkedin</a><br/>
-            <a href="https://github.com/jkevintu">>> Github</a><br/>
-            <a href="https://stackoverflow.com/users/527376/ktu">>> Stackoverflow</a>
-          </p>
-        </Section>
-        <Section><hr/></Section>
-        <Section sectionId="projects">
-          <h2>Projects</h2>
-          <div className="section-items">
-            <ImageItem name="Gamer Sensei" subName="ecommerce"></ImageItem>
-            <ImageItem name="Liaison"  subName="508 Compliance"></ImageItem>
-            <ImageItem name="Pothole Sonar"></ImageItem>
-          </div>
-        </Section>
-        <Section><hr/></Section>
-        <Section sectionId="designs">
-          <h2>Designs</h2>
-          <div className="section-items">
-            <ImageItem name="Poster"></ImageItem>
-            <ImageItem name="Event Banner"></ImageItem>
-          </div>
-        </Section>
-      </Layout>
-    </div>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <Router>
+      <Route path="/" component={Home} />
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Layout from "../components/Layout"
+import Hero from "../components/Hero"
+import Section from "../components/Section"
+import ImageItem from "../components/ImageItem"
+
+export default function Home() {
+  return (
+    <Layout>
+      <Hero isHomepage="true"></Hero>
+      <Section sectionId="about">
+        <h2>About</h2>
+        <code>
+          <strong>J.Kevin Tu</strong><br/>
+          Frontend Developer / Consultant<br/>
+          -----------------------------------<br/>
+          HTML/CSS<br/>
+          Javascript (Vue, React, Angular)<br/>
+          UI/UX/Branding (Photoshop, Sktech)<br/>
+          SEO, SEM, Google Analytics<br/>
+          508 Compliance<br/>
+        </code>
+        <p>
+          <a href="mailto:ktu@jkevintu.com">ktu@jkevintu.com</a><br/><br/>
+          <a href="https://www.linkedin.com/in/jkevintu">>> linkedin</a><br/>
+          <a href="https://github.com/jkevintu">>> Github</a><br/>
+          <a href="https://stackoverflow.com/users/527376/ktu">>> Stackoverflow</a>
+        </p>
+      </Section>
+      <Section><hr/></Section>
+      <Section sectionId="projects">
+        <h2>Projects</h2>
+        <div className="section-items">
+          <ImageItem name="Gamer Sensei" subName="ecommerce"></ImageItem>
+          <ImageItem name="Liaison"  subName="508 Compliance"></ImageItem>
+          <ImageItem name="Pothole Sonar"></ImageItem>
+        </div>
+      </Section>
+      <Section><hr/></Section>
+      <Section sectionId="designs">
+        <h2>Designs</h2>
+        <div className="section-items">
+          <ImageItem name="Poster"></ImageItem>
+          <ImageItem name="Event Banner"></ImageItem>
+        </div>
+      </Section>
+    </Layout>
+  )
+}
