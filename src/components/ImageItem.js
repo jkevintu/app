c9f7790b71c93fe4cf6bbac5a0d8e74df2b832f7
Kevin Tu <ktu@jkevintu.com>
2020-08-15 09:36:03 +0800

    Update componenets into useState

--- src/components/ImageItem.js
+++ src/components/ImageItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import "./ImageItem.scss";
@@ -14,30 +14,28 @@ const getSubname = (props) => {
 const getImgHoverClass = (state) => {
   if (state.hover) return "hover";
 }
-class ImageItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hover: false };
-    this.props = props;
+
+const ImageItem = (props)  => {
+  const [hover, setHover] = useState(false);
+
+  function toggleHover (hoverStatus) {
+    setHover(hoverStatus);
   }
-  toggleHover (hoverStatus) {
-    this.setState({ hover: hoverStatus });
-  }
-  render() {
-    return (
-      <div className="image-item"
-           onMouseEnter={() => this.toggleHover(true)}
-           onMouseLeave={() => this.toggleHover(false)}>
-        <Link to={this.props.url || "#"}>
-          <div className="image-item-name-wrapper">
-              {this.props.name}
-              {getSubname(this.props)}
-          </div>
-          <img className={getImgHoverClass(this.state)} src={this.props.imgSrc || DEFAULT_IMG} alt={this.props.name}></img>
-        </Link>        
-      </div>
-    )  
-  }
+  return (
+    <div className="image-item"
+          onMouseEnter={() => toggleHover(true)}
+          onMouseLeave={() => toggleHover(false)}>
+      <Link to={props.url || "#"}>
+        <div className="image-item-name-wrapper">
+            {props.name}
+            {getSubname(props)}
+        </div>
+        <img className={getImgHoverClass({hover: hover})}
+             src={props.imgSrc || DEFAULT_IMG}
+             alt={props.name}></img>
+      </Link>
+    </div>
+  )
 }
 
 export default ImageItem;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,16 @@
 import React from "react";
 import "./Section.scss";
 
-class Section extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hover: false };
-    this.props = props;
-    const { className = "" } = props;
-    this.classes = "section " + className;
-  }
-  render() {
-    return (
-      <div id={this.props.sectionId} className={this.classes}>
-        {this.props.children}
-      </div>
-    )
-  }
+function Section (props) {
+
+  const { className = "" } = props;
+  const classes = "section " + className;
+
+  return (
+    <div id={props.sectionId} className={classes}>
+      {props.children}
+    </div>
+  )
 }
 
 export default Section;
