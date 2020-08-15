01cfe3d73984ac1ce04dd43cd1eb238620a9af4b
Kevin Tu <ktu@jkevintu.com>
2020-08-15 22:12:33 +0800

    Quifforce page - final
    Update image content / designs name

--- src/pages/Quikforce.js
+++ src/pages/Quikforce.js
@@ -13,6 +13,8 @@ import QuikforceImg from '../img/project-quikforce.png';
 import QuikforceImg1 from '../img/project-quikforce-booking-desktop.jpg';
 import QuikforceImg2 from '../img/project-quikforce-booking-mobile.jpg';
 import QuikforceImg3 from '../img/project-quikforce-3.jpg';
+import QuikforceImg4 from '../img/project-quikforce-4.png';
+import QuikforceImg5 from '../img/project-quikforce-5.png';
 
 
 export default function Quikforce(props) {
@@ -49,50 +51,66 @@ export default function Quikforce(props) {
         </SectionProjectDesc>
       </Section>
       <Section className="flex">
-        <SectionProfileImage name="Structure" imgURL={QuikforceImg1}/>
+        <SectionProfileImage name="Quikforce website on desktop" imgURL={QuikforceImg1}/>
       </Section>
       <div className="section-wrapper dark">
         <Section className="dark flex">
           <SectionProfileCard>
-            <div className="section-profile-title">Problem</div>
-            <div className="section-profile-subtitle">Lorem ipsum dolor sit amet</div>
+            <div className="section-profile-title">
+              Problem
+            </div>
+            <div className="section-profile-subtitle">
+              Moving is painful
+            </div>
             <div className="section-profile-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              Prior the moving, movers will send a guy to estimate how long is the moving and send you a quote, however the quote is not final and can be changed due to many reasons. Also,  many movers still only accept cash payments
             </div>
           </SectionProfileCard>
           <SectionProfileCard>
             <div className="section-profile-title">Thinking</div>
-            <div className="section-profile-subtitle">Lorem ipsum dolor sit amet</div>
+            <div className="section-profile-subtitle">
+              How to resolve user's pain points using technologies?
+            </div>
             <div className="section-profile-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              For movers, the major factor that affect total moving cost is by<i>Time</i>, but a lot of the movers chose to not disclose this information. The lack of transparenncy and uncertainty often makes movees uncomfortable and unhappy about the total moving experience
             </div>
           </SectionProfileCard>
           <SectionProfileCard>
-            <div className="section-profile-title">Result</div>
-            <div className="section-profile-subtitle">Lorem ipsum dolor sit amet</div>
+            <div className="section-profile-title">
+              Solution
+            </div>
+            <div className="section-profile-subtitle">
+              One-stop-shop for both movers and movees
+            </div>
             <div className="section-profile-content">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              We built and provide an online quoting and scheduling software for people who want to move, also we created a portal for movers to accept / manage their moving schedules
             </div>
           </SectionProfileCard>
         </Section>
       </div>
       <Section>
-        <SectionProfileImage name="Structure" imgURL={QuikforceImg2}/>
+        <SectionProfileImage name="Moving estimate and booking software" imgURL={QuikforceImg5}/>
         <SectionProfileWrapper>
           <SectionProfileDetail className="title">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            Adpatie and adjust from suggestions from real customers
           </SectionProfileDetail>
           <SectionProfileDetail className="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p>
+            <p>It is almost impossible to learn about moving industry if sitting at home, before the product launch we need to find the partners that are interested working with us and learn how does the current moving workflow looks like.</p>
+            <p>After went on 100+ moves, we collect feedbacks and suggesiton from actual cusomters and movers, and improve our product from their feedback. </p>
+            <p>We end have having 200+ moves, over 100k revenues within 3 months.</p>
           </SectionProfileDetail>
+        </SectionProfileWrapper>
+        <SectionProfileWrapper>
+          <SectionProfileImage name="Mover's dashboard" imgURL={QuikforceImg4}/>
+        </SectionProfileWrapper>
+        <SectionProfileWrapper>
+          <SectionProfileImage name="Quikforce website on mobile" imgURL={QuikforceImg2}/>
         </SectionProfileWrapper>
         <SectionProfileWrapper>
           <iframe title="video" width="100%" height="630px" src="https://www.youtube.com/embed/TzWd8Vgsabw" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </SectionProfileWrapper>
         <SectionProfileWrapper>
-          <SectionProfileImage name="Structure" imgURL={QuikforceImg3}/>
+          <SectionProfileImage name="Quikforce moving" imgURL={QuikforceImg3}/>
         </SectionProfileWrapper>
       </Section>
 
