6490346b5f5049b56abad1a4c89d74802f5c98ab
Kevin Tu <ktu@jkevintu.com>
2020-08-15 10:25:45 +0800

    Update meta tag
    Update metatag

--- src/components/ProjectLayout.js
+++ src/components/ProjectLayout.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header"
+import Footer from "./Footer"
 
 export default function ProjectLayout(props) {
   return (
     <div className="project-layout">
       <Header isHomePage={false} />
       {props.children}
+      <Footer></Footer>
     </div>
   )
 }
