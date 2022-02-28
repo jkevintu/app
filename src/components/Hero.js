94c7705f8645bc4e96b76c2ba96907ecd1b8f83b
Kevin Tu <ktu@jkevintu.com>
2022-02-28 11:52:21 +0800

    Parelle scrolling in project page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,59 +1,64 @@
 import React from "react";
-import "./Hero.scss"
+import "./Hero.scss";
 
 const homePageSize = {
-  minHeight: "100vh"
-}
+  minHeight: "100vh",
+  width: "100%",
+};
 
 const projectPageSize = {
   height: "670px",
-  maxHeight: '75vh',
+  maxHeight: "75vh",
   color: "white",
-}
+};
 
 const textDefaultStyle = {
-  position: 'absolute',
+  position: "absolute",
   zIndex: 1,
-  width: '100%',
+  width: "100%",
   display: "flex",
   flexDirection: "column",
   alignItems: "center",
   justifyContent: "center",
   color: "white",
-  textShadow: '1px 1px #888'
-}
+  textShadow: "1px 1px #888",
+};
 
 const getTextStyle = (props) => {
-  let coverStyle = (props.isHomePage) ? homePageSize : projectPageSize;
-  let style = Object.assign({position: 'absolute'}, textDefaultStyle, coverStyle);
+  let coverStyle = props.isHomePage ? homePageSize : projectPageSize;
+  let style = Object.assign(
+    { position: "absolute" },
+    textDefaultStyle,
+    coverStyle
+  );
   return style;
-}
+};
 
 const getCoverStyle = (props) => {
-  let coverStyle = (props.isHomePage) ? homePageSize : projectPageSize;
-  let style = Object.assign({}, coverStyle, getBackground(props))
+  let coverStyle = props.isHomePage ? homePageSize : projectPageSize;
+  let style = Object.assign({}, coverStyle, getBackground(props));
   return style;
-}
+};
 
 const getBackground = (props) => {
   let style = {};
-  let heroImgUrl = "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAGCAYAAADgzO9IAAAAOElEQVQIW2MUFBT8//79ewYQALLBNIjP6OLiApe4d+8eg5KSEsPZs2chEiAGSIB4CZgdKDpwWQ4A7Mc2AV5T93AAAAAASUVORK5CYII=) repeat";
+  let heroImgUrl =
+    "url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAYAAAAGCAYAAADgzO9IAAAAOElEQVQIW2MUFBT8//79ewYQALLBNIjP6OLiApe4d+8eg5KSEsPZs2chEiAGSIB4CZgdKDpwWQ4A7Mc2AV5T93AAAAAASUVORK5CYII=) repeat";
   if (props.heroImgUrl) {
-    heroImgUrl = `url(${props.heroImgUrl}) center center / cover`
+    heroImgUrl = `url(${props.heroImgUrl}) center center / cover`;
     // style.filter = "contrast(30%)";
   }
   style.background = heroImgUrl;
+  style.backgroundAttachment = "fixed";
+
   return style;
-}
+};
 
 export default function Hero(props) {
   return (
     <div className="hero">
-      <div style={getTextStyle(props)}>
-        {props.children}
-      </div>
-      <div style={getCoverStyle(props)}>
-      </div>
+      <div style={getTextStyle(props)}>{props.children}</div>
+      <div style={getCoverStyle(props)}></div>
     </div>
-  )
+  );
 }
